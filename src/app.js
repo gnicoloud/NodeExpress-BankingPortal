07722fb5ca94c2095ee9689428d842d42b952a49
gnicoloud <gnicoloud@gmail.com>
2018-10-02 16:29:22 +0200

    fix

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const { accounts, users, writeJSON } = require('./data');
 const accountRoutes = require('./routes/accounts');
-const serviceRoutes = require('./routes/services');
+const servicesRoutes = require('./routes/services');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'Account Summary', accounts})
 });
 app.use('/account', accountRoutes);
-app.use('/services', serviceRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] });
